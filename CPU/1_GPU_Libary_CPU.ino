5c4c7610fd474fc42849e04303158cb5135e4ef3
ldev <ldev@ldev.eu.org>
2023-05-26 17:52:02 +0200

    changed some stuff (no complie time err)

---
+++ CPU/1_GPU_Libary_CPU.ino
@@ -0,0 +1,42 @@
+const int ResetGpu_pin = 4;
+
+struct GTexture
+{
+  byte Width;
+  byte Height;
+
+  bool* Data; 
+};
+
+void gpu_init(){
+ Serial.println("initializing gpu");
+ pinMode(ResetGpu_pin, OUTPUT);
+ 
+ digitalWrite(ResetGpu_pin, HIGH);
+ delay(100);
+ digitalWrite(ResetGpu_pin, LOW);
+ delay(3000);
+ Serial.println("gpu initialized");
+
+ com_init();
+}
+
+struct GTexture *gpu_createTex(byte w, byte h)
+{
+  struct GTexture *tex;
+  tex = (GTexture*)malloc(sizeof(struct GTexture));
+  tex->Width = w;
+  tex->Height = h;
+  tex->Data = (bool*)malloc(sizeof(bool)*w*h);
+  return tex;
+}
+
+
+void gpu_uploadTex(struct GTexture* tex)
+{
+  com_sendByte(tex->Width, 3);
+  com_sendByte(tex->Height, 3);
+  for (int i=0; i<tex->Width*tex->Height; i++){
+    com_sendBit(tex->Data[i]);
+  }
+}
