945a503e4d9dd5f85580cac95b89a6f000c0cc90
ldev <ldev@ldev.eu.org>
2023-05-28 09:44:43 +0200

    changed prints

--- CPU/CPU.ino
+++ CPU/CPU.ino
@@ -36,70 +36,94 @@ void loop() {
 
 
   if (y > 700 && x < 200) {
-    Serial.print("TopLeft");
+    
     Direction = 2;
     if (DISx > 0 && DISy > 0) {
+      Serial.print("TopLeft");
       DISx += -1;
       DISy += -1;
       on_input();
+    }else{
+      Serial.println("TopLeft limit reached");
     }
   }
   if (y > 200 && y < 700 && x < 200) {
-    Serial.print("Top");
+    
     Direction = 1;
     if (DISy > 0) {
+      Serial.print("Top");
       DISy += -1;
+    }else{
+      Serial.println("Top limit reached");
     }
   }
   if (x < 200 && y < 200) {
-    Serial.print("TopRight");
+   
     Direction = 8;
     if (DISx < 7 && DISy > 0) {
+      Serial.print("TopRight");
       DISx += 1;
       DISy += -1;
       on_input();
+    }else{
+      Serial.println("TopRight limit reached");
     }
   }
   if (x > 200 && x < 700 && y < 200) {
-    Serial.print("Right");
+   
     Direction = 7;
     if (DISx < 7) {
+      Serial.print("Right");
       DISx += 1;
       on_input();
+    }else{
+      Serial.println("Right limit reached");
     }
   }
   if (y < 200 && x > 700) {
-    Serial.print("BottomRight");
+    
     Direction = 6;
     if (DISx < 7 && DISy < 7) {
+      Serial.print("BottomRight");
       DISx += 1;
       DISy += 1;
       on_input();
+    }else{
+      Serial.println("BottomRight limit reached");
     }
   }
   if (y > 200 && y < 700 && x > 700) {
-    Serial.print("Down");
+    
     Direction = 5;
     if (DISy < 7) {
+      Serial.print("Down");
       DISy += 1;
       on_input();
+    }else{
+      Serial.println("Down limit reached");
     }
   }
   if (y > 700 && x > 700) {
-    Serial.print("BottomLeft");
+   
     Direction = 4;
     if (DISx > 0 && DISy < 7) {
+      Serial.print("BottomLeft");
       DISx += -1;
       DISy += 1;
       on_input();
+    }else{
+      Serial.println("BottomLeft limit reached");
     }
   }
   if (x > 200 && x < 700 && y > 700) {
-    Serial.print("Left");
+    
     Direction = 3;
     if (DISx > 0) {
+      Serial.print("Left");
       DISx += -1;
       on_input();
+    }else{
+      Serial.println("Left limit reached");
     }
   }
   if (Button == 0 && x < 600 && x > 400 && y < 600 && y > 400) {
