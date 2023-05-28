478625c264de5b4a686bbcdaa694c1e0bfe57ae4
ldev <ldev@ldev.eu.org>
2023-05-28 11:35:50 +0200

    changed speed to 5bps

--- CPU/0_CPU_com.ino
+++ CPU/0_CPU_com.ino
@@ -2,7 +2,7 @@
 const int ComPin = 3;
 const int ClockPin = 2;
 
-const float ClockSpeed = 30; // bits per second(bps)
+const float ClockSpeed = 5; // bits per second(bps)
 
 const int ClockDelay = (int)(1000.0/ClockSpeed); //Amount of delay between pulsing the clock
 
