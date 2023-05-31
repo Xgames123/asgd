5dc671f5b29c28fd8a58767e8a06561c27bdadb9
ldev <ldev@ldev.eu.org>
2023-05-31 22:14:40 +0200

    forgot return again

--- CPU/1_GPU_Libary_CPU.ino
+++ CPU/1_GPU_Libary_CPU.ino
@@ -40,16 +40,7 @@ struct GTexture *gpu_createTex(byte w, byte h)
   tex->Height = h;
   tex->Data = (bool*)malloc(sizeof(bool)*w*h);
   
-  byte ww = tex->Width;
-  byte hh = tex->Height;
-
-  Serial.print("gpu_createTex_after (");
-  Serial.print("w: ");
-  Serial.print(ww);
-  Serial.print(", h: ");
-  Serial.print(hh);
-  Serial.println(" )");
-
+  return tex;
 }
 
 
